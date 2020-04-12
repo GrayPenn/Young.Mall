5253e4515d2437ac9c3e3dde33fc9459991008c0
Young <421813163@qq.com>
2020-04-12 20:49:49 +0800

    订单模块

--- src/main/java/com.young.mall/config/UserArgumentResolver.java
+++ src/main/java/com.young.mall/config/UserArgumentResolver.java
@@ -14,6 +14,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Service
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
+	
 	@Autowired
 	MallUserService userService;
 
